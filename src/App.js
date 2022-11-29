d78e37ba926f7d8f1903124a2250473868781a13
jiii-9 <succeed.g9@gmail.com>
2022-11-29 20:06:41 +0900

    add react-router-dom and components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainHeader from "./components/Layout/MainHeader";
+import SearchForm from "./components/Search/SearchForm";
+
+import "./App.css";
+import Recipe from "./components/Recipe/Recipe";
 
 function App() {
+  const API_KEY =
+    "JFTV7vhRItec0ldhlHpK1L9oaCQKJGWYqzw54%2F6Kfjo0Dor9L%2BwFgvS%2FmBbVM3roQZf1q8yCiQoRqFbP2dEBfQ%3D%3D";
+  const url = `http://apis.data.go.kr/1471000/FoodNtrIrdntInfoService1/getFoodNtrItdntList1?ServiceKey=${API_KEY}&desc_kor=양파&type=json`;
+
+  const getData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log("data", data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inner">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainHeader />} />
+          </Routes>
+        </BrowserRouter>
+        <main>
+          <SearchForm />
+          <Recipe />
+        </main>
+      </div>
     </div>
   );
 }
