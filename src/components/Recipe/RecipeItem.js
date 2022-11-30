cf7c39dc0bf04740b327fbe57cf3c9cc70561ea4
jiii-9 <succeed.g9@gmail.com>
2022-11-30 20:12:36 +0900

    create a list with the received recipe data

--- src/components/Recipe/RecipeItem.js
+++ src/components/Recipe/RecipeItem.js
@@ -5,14 +5,10 @@ import classes from "./RecipeItem.module.css";
 const RecipeItem = props => {
   return (
     <Fragment>
-      <li className={classes["recipe-item"]}></li>
-      <li className={classes["recipe-item"]}></li>
-      <li className={classes["recipe-item"]}></li>
-      <li className={classes["recipe-item"]}></li>
-      <li className={classes["recipe-item"]}></li>
-      <li className={classes["recipe-item"]}></li>
-      <li className={classes["recipe-item"]}></li>
-      <li className={classes["recipe-item"]}></li>
+      <li className={classes["recipe-item"]}>
+        <img className={classes["recipe-image"]} src={`${props.mainImage}`} />
+        <span className={classes["recipe-name"]}>{props.name}</span>
+      </li>
     </Fragment>
   );
 };
