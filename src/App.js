4bbe3a6d5067ead3f8bba90eba949ef0037e1ae9
jiii-9 <succeed.g9@gmail.com>
2022-11-30 20:09:15 +0900

    connecting recipe data to context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainHeader from "./components/Layout/MainHeader";
 import SearchForm from "./components/Search/SearchForm";
+import Navigation from "./components/Layout/Navigation";
 
 import "./App.css";
 import Recipe from "./components/Recipe/Recipe";
 
+export const RecipeContext = React.createContext();
 function App() {
-  const API_KEY =
-    "JFTV7vhRItec0ldhlHpK1L9oaCQKJGWYqzw54%2F6Kfjo0Dor9L%2BwFgvS%2FmBbVM3roQZf1q8yCiQoRqFbP2dEBfQ%3D%3D";
-  const url = `http://apis.data.go.kr/1471000/FoodNtrIrdntInfoService1/getFoodNtrItdntList1?ServiceKey=${API_KEY}&desc_kor=양파&type=json`;
+  const [recipeData, setRecipeData] = useState([]);
 
-  const getData = async () => {
-    const response = await fetch(url);
-    const data = await response.json();
+  // const MATERIAL_API_KEY =
+  //   "JFTV7vhRItec0ldhlHpK1L9oaCQKJGWYqzw54%2F6Kfjo0Dor9L%2BwFgvS%2FmBbVM3roQZf1q8yCiQoRqFbP2dEBfQ%3D%3D";
+  // const material_url = `http://apis.data.go.kr/1471000/FoodNtrIrdntInfoService1/getFoodNtrItdntList1?ServiceKey=${MATERIAL_API_KEY}&desc_kor=양파&type=json`;
 
-    console.log("data", data);
-  };
+  // const getMaterialData = async () => {
+  //   const response = await fetch(material_url);
+  //   const data = await response.json();
+
+  //   console.log("data", data);
+  // };
+
+  // Recipe data api
+  const RECIPE_API_KEY = "ec08a6953db24a9dbfbb";
+  const recipe_url = `http://openapi.foodsafetykorea.go.kr/api/${RECIPE_API_KEY}/COOKRCP01/json/1/100/RCP_NM="샐러드"`;
+
+  useEffect(() => {
+    setTimeout(() => {
+      const getRecipeData = async () => {
+        const response = await fetch(recipe_url);
+        const data = await response.json();
+
+        setRecipeData(data);
+      };
+      getRecipeData();
+    }, 200);
+  }, []);
 
   return (
-    <div className="App">
+    <RecipeContext.Provider value={recipeData}>
       <div className="inner">
         <BrowserRouter>
           <Routes>
@@ -31,7 +51,8 @@ function App() {
           <Recipe />
         </main>
       </div>
-    </div>
+      <Navigation />
+    </RecipeContext.Provider>
   );
 }
 
