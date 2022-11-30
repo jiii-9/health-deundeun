e9222f8b1ae57de3693e5f1f7ba2103abda7fd22
jiii-9 <succeed.g9@gmail.com>
2022-11-30 20:11:20 +0900

    distributed as a list of received recipe data

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import { RecipeContext } from "../../App";
 
 import classes from "./Recipe.module.css";
 import RecipeItem from "./RecipeItem";
 
 const Recipe = props => {
+  const recipeCtx = useContext(RecipeContext);
+  const recipes = recipeCtx.COOKRCP01.row;
+
+  console.log(recipeCtx.COOKRCP01.row);
+
   return (
     <section className={classes.recipe}>
       <span className={classes["recipe-title"]}>인기있는 레시피</span>
       <ul className={classes["recipe-list"]}>
-        <RecipeItem />
+        {recipes.map(recipe => (
+          <RecipeItem
+            key={recipe.RCP_SEQ}
+            name={recipe.RCP_NM}
+            mainImage={recipe.ATT_FILE_NO_MAIN}
+          />
+        ))}
       </ul>
     </section>
   );
