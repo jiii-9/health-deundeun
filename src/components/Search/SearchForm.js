98e248d88a9cc5ae5a02e57dcc9b5f0554cecbaf
jiii-9 <succeed.g9@gmail.com>
2022-12-13 18:24:22 +0900

    add a function to enable two-way binding with the nutrition information context

--- src/components/Search/SearchForm.js
+++ src/components/Search/SearchForm.js
@@ -1,38 +1,66 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import Input from "./Input";
+import { MaterialContext } from "../store/Material";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 import classes from "./SearchForm.module.css";
 
 const SearchForm = () => {
+  // materialData context 사용
+  const MaterialContextProvider = useContext(MaterialContext);
+
+  // input에 입력한 value값
+  const [searchMaterial, setSearchMaterial] = useState("");
+
   const navigate = useNavigate();
-  const [enteredSearchItem, setEnteredSearchItem] = useState("");
 
-  const extendSearchItemHandler = enteredItem => {
-    // input에서 받아온 인자를 enteredSearchItem에 넣어줌
-    setEnteredSearchItem(enteredItem);
-  };
-  console.log(enteredSearchItem);
-
+  // 버튼 클릭 시 이동해주는 함수
   const moveResultHandler = () => {
-    navigate(`/result/${enteredSearchItem}`);
+    if (searchMaterial.length > 0) {
+      navigate(`/result/${searchMaterial}`);
+    }
+    return;
   };
 
   const submitHandler = event => {
     event.preventDefault();
+    MaterialContextProvider.changeUrlHandler(searchMaterial);
 
-    if (enteredSearchItem.length === 0) {
+    if (searchMaterial.length === 0) {
       window.confirm("재료를 입력하세요🥬");
     } else {
       moveResultHandler(); // result 페이지로 이동
     }
   };
 
+  const searchItemHandler = e => {
+    setSearchMaterial(e.target.value);
+  };
+
   return (
     <div className="inner">
       <span className={classes["search-title"]}>오늘도 건강하게!</span>
       <form className={classes["search-form"]} onSubmit={submitHandler}>
-        <Input onChange={extendSearchItemHandler} onMove={submitHandler} />
+        <div className={classes["input-wrapper"]}>
+          <label htmlFor="search"></label>
+          <FontAwesomeIcon className={classes["search-icon"]} icon={faSearch} />
+          <input
+            id="search"
+            className={classes.input}
+            type="text"
+            placeholder="재료를 입력하세요"
+            onChange={searchItemHandler}
+          />
+          <button
+            className={classes["search-btn"]}
+            onClick={submitHandler}
+            type="button"
+          >
+            검색
+          </button>
+        </div>
       </form>
     </div>
   );
