bd6c00cf61ad52c038c5291b7d4e8bcf422f2d1f
jiii-9 <succeed.g9@gmail.com>
2022-12-13 18:56:01 +0900

    create the resultItem component

--- src/components/Result/ResultItem.js
+++ src/components/Result/ResultItem.js
@@ -1,25 +1,9 @@
-import React, { useContext } from "react";
-import { MaterialContext } from "../store/Material";
+import React from "react";
 
-const ResultItem = () => {
-  const MaterialContextProvider = useContext(MaterialContext);
-  const receivedData = MaterialContextProvider.materialData.I2790;
-  const dataResultItems = receivedData.row;
-
-  if (receivedData.total_count === "0") {
-    return null;
-  }
-
+const ResultItem = props => {
   return (
     <div>
-      <h1>재료 검색 결과 화면</h1>
-
-      {dataResultItems.map(item => (
-        <div key={item.FOOD_CD}>
-          <span>{item.DESC_KOR}</span>
-          <span>{item.NUTR_CONT1}</span>
-        </div>
-      ))}
+      <span>{props.name}</span>
     </div>
   );
 };
