f3ba4e6306e00d064a80c0a31bbbfd4b78c76e44
jiii-9 <succeed.g9@gmail.com>
2022-12-13 18:21:53 +0900

    receive nutritional data

--- src/components/Result/ResultItem.js
+++ src/components/Result/ResultItem.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MaterialContext } from "../store/Material";
 
 const ResultItem = () => {
+  const MaterialContextProvider = useContext(MaterialContext);
+  const receivedData = MaterialContextProvider.materialData.I2790;
+  const dataResultItems = receivedData.row;
+
+  if (receivedData.total_count === "0") {
+    return null;
+  }
+
   return (
     <div>
       <h1>재료 검색 결과 화면</h1>
+
+      {dataResultItems.map(item => (
+        <div key={item.FOOD_CD}>
+          <span>{item.DESC_KOR}</span>
+          <span>{item.NUTR_CONT1}</span>
+        </div>
+      ))}
     </div>
   );
 };
