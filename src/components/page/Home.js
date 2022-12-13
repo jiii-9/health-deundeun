a348dc6ce60c231131d695c9f5c17b224b739c77
jiii-9 <succeed.g9@gmail.com>
2022-12-13 18:28:05 +0900

    no change

--- src/components/page/Home.js
+++ src/components/page/Home.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import Recipe from "../Recipe/Recipe";
 import SearchForm from "../Search/SearchForm";
-import { MaterialContext } from "../store/Material";
 
 const Home = () => {
   return (
