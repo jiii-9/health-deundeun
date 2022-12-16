423e3f034a18a60131a5e4412d1d598005f754ca
jiii-9 <succeed.g9@gmail.com>
2022-12-16 16:32:39 +0900

    add back button and like button component

--- src/components/Recipe/RecipeManual.js
+++ src/components/Recipe/RecipeManual.js
@@ -1,46 +1,45 @@
 import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
-
-import classes from "./RecipeManual.module.css";
+import { useLocation } from "react-router-dom";
 import BackButton from "../UI/BackButton";
 
-const RecipeManual = () => {
-  const navigate = useNavigate();
-  const { state } = useLocation();
+import classes from "./RecipeManual.module.css";
+import HeartButton from "../UI/HeartButton";
 
-  const goBackHandler = () => {
-    navigate(-1);
-  };
+const RecipeManual = () => {
+  const { state } = useLocation();
 
   console.log(state.recipes);
 
   return (
     <div className={classes.recipe}>
-      <BackButton onClick={goBackHandler}>
-        <FontAwesomeIcon className={classes["back-icon"]} icon={faAngleLeft} />
-        <h2 className={classes["menu-name"]}>{state.name}</h2>
-      </BackButton>
+      <div className="inner">
+        <BackButton>
+          <h2 className={classes["menu-name"]}>{state.name}</h2>
+        </BackButton>
 
-      <div className={classes["recipe-wrapper"]}>
-        <h3 className={classes["menu-material-title"]}>재료</h3>
-        <span className={classes["menu-material"]}>{state.ingredient}</span>
-        {state.recipes.map((recipe, idx) => (
-          <div className={classes["menu-item"]} key={idx}>
-            {recipe.recipeImage !== "" && (
-              <img
-                className={classes["recipe-image"]}
-                src={recipe.recipeImage}
-                alt="재료 사진"
-              />
-            )}
-            {recipe.recipeDes !== "" && (
-              <span className={classes["recipe-des"]}>{recipe.recipeDes}</span>
-            )}
+        <div className={classes["recipe-wrapper"]}>
+          <div className={classes["menu-material-wrapper"]}>
+            <h3 className={classes["menu-material-title"]}>재료</h3>
+            <span className={classes["menu-material"]}>{state.ingredient}</span>
           </div>
-        ))}
+          {state.recipes.map((recipe, idx) => (
+            <div className={classes["menu-item"]} key={idx}>
+              {recipe.recipeImage !== "" && (
+                <img
+                  className={classes["recipe-image"]}
+                  src={recipe.recipeImage}
+                  alt="재료 사진"
+                />
+              )}
+              {recipe.recipeDes !== "" && (
+                <span className={classes["recipe-des"]}>
+                  {recipe.recipeDes}
+                </span>
+              )}
+            </div>
+          ))}
+          <HeartButton />
+        </div>
       </div>
     </div>
   );
