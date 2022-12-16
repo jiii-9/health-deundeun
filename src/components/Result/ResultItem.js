818700c2c60c480e686c306049e0b72b20ad9385
jiii-9 <succeed.g9@gmail.com>
2022-12-16 16:37:50 +0900

    create nutritional information item component

--- src/components/Result/ResultItem.js
+++ src/components/Result/ResultItem.js
@@ -1,9 +1,55 @@
 import React from "react";
 
-const ResultItem = props => {
+import classes from "./ResultItem.module.css";
+
+const ResultItem = ({
+  name,
+  kcal,
+  carbohydrate,
+  protein,
+  fat,
+  sugars,
+  sodium,
+  cholesterol,
+  transFat,
+}) => {
   return (
-    <div>
-      <span>{props.name}</span>
+    <div className={classes["result-item"]}>
+      <span className={classes["food-name"]}>{name}</span>
+      <div className={classes["result-info"]}>
+        <div className={classes["item-info"]}>
+          <span className={classes["nutrient-name"]}>열량</span>
+          <span className={classes.amount}>{kcal}</span>
+        </div>
+        <div className={classes["item-info"]}>
+          <span className={classes["nutrient-name"]}>탄수화물</span>
+          <span className={classes.amount}>{carbohydrate}</span>
+        </div>
+        <div className={classes["item-info"]}>
+          <span className={classes["nutrient-name"]}>단백질</span>
+          <span className={classes.amount}>{protein}</span>
+        </div>
+        <div className={classes["item-info"]}>
+          <span className={classes["nutrient-name"]}>지방</span>
+          <span className={classes.amount}>{fat}</span>
+        </div>
+        <div className={classes["item-info"]}>
+          <span className={classes["nutrient-name"]}>당류</span>
+          <span className={classes.amount}>{sugars}</span>
+        </div>
+        <div className={classes["item-info"]}>
+          <span className={classes["nutrient-name"]}>나트륨</span>
+          <span className={classes.amount}>{sodium}</span>
+        </div>
+        <div className={classes["item-info"]}>
+          <span className={classes["nutrient-name"]}>콜레스테롤</span>
+          <span className={classes.amount}>{cholesterol}</span>
+        </div>
+        <div className={classes["item-info"]}>
+          <span className={classes["nutrient-name"]}>트렌스지방</span>
+          <span className={classes.amount}>{transFat}</span>
+        </div>
+      </div>
     </div>
   );
 };
