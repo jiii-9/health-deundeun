89eafba21d34d3cd4a05e85f0b97ef6cfc644861
jiii-9 <succeed.g9@gmail.com>
2022-12-16 16:39:11 +0900

    create nutritional information list component

--- src/components/Result/ResultList.js
+++ src/components/Result/ResultList.js
@@ -1,26 +1,47 @@
 import React, { useContext } from "react";
-import ResultItem from "./ResultItem";
 import { MaterialContext } from "../store/Material";
+import ResultItem from "./ResultItem";
+import BackButton from "../UI/BackButton";
+
+import classes from "./ResultList.module.css";
+import HeartButton from "../UI/HeartButton";
 
 const ResultList = () => {
   const MaterialContextProvider = useContext(MaterialContext);
   const receivedData = MaterialContextProvider.materialData.I2790;
   const dataResultItems = receivedData.row;
+  const enteredValue = MaterialContextProvider.enteredInputValue;
 
   if (receivedData.total_count === "0") {
     return null;
   }
 
   return (
-    <div>
-      <h1>재료 검색 결과 화면</h1>
-
-      {dataResultItems.map(item => (
-        <div key={item.FOOD_CD}>
-          <ResultItem name={item.DESC_KOR} />
-        </div>
-      ))}
-    </div>
+    <section className={classes["result-list"]}>
+      <div className="inner">
+        <BackButton>
+          <span className={classes["result-title"]}>
+            '{enteredValue}' 검색 결과
+          </span>
+        </BackButton>
+        <HeartButton />
+        {dataResultItems.map(item => (
+          <div key={item.FOOD_CD}>
+            <ResultItem
+              name={item.DESC_KOR}
+              kcal={item.NUTR_CONT1}
+              carbohydrate={item.NUTR_CONT2}
+              protein={item.NUTR_CONT3}
+              fat={item.NUTR_CONT4}
+              sugars={item.NUTR_CONT5}
+              sodium={item.NUTR_CONT6}
+              cholesterol={item.NUTR_CONT7}
+              transFat={item.NUTR_CONT9}
+            />
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
 
