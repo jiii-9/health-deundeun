3616993a2beafc403ae383debb186e9f4a200307
jiii-9 <succeed.g9@gmail.com>
2022-12-16 16:40:49 +0900

    add a function to go back

--- src/components/UI/BackButton.js
+++ src/components/UI/BackButton.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 
 import classes from "./BackButton.module.css";
 
 const BackButton = props => {
+  const navigate = useNavigate();
+
+  const goBackHandler = () => {
+    navigate(-1);
+  };
+
   return (
-    <div onClick={props.onClick} className={classes.back}>
+    <button onClick={goBackHandler} className={classes.back}>
+      <FontAwesomeIcon className={classes["back-icon"]} icon={faAngleLeft} />
       {props.children}
-    </div>
+    </button>
   );
 };
 
