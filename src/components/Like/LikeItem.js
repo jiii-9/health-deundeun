27f5d46ce366e474b022ee3cff7f311e97b81940
jiii-9 <succeed.g9@gmail.com>
2022-12-20 18:36:30 +0900

    create the components of the like page

---
+++ src/components/Like/LikeItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LikeItem = () => {
+  return (
+    <div>
+      <h2>Like Item</h2>
+    </div>
+  );
+};
+
+export default LikeItem;
---
+++ src/components/Like/LikeList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import LikeItem from "./LikeItem";
+
+const LikeList = () => {
+  return (
+    <div>
+      <h1>Like List</h1>
+      <LikeItem />
+    </div>
+  );
+};
+
+export default LikeList;
