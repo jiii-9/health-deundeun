a5117644f2d4cceacf858ab7b793f53e09272e48
jiii-9 <succeed.g9@gmail.com>
2022-12-20 18:34:40 +0900

    add components to the likes page

--- src/components/page/Like.js
+++ src/components/page/Like.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from "react";
+import LikeList from "../Like/LikeList";
 
 const Like = () => {
   return (
     <Fragment>
-      <h1>좋아요 페이지</h1>
+      <div className="inner">
+        <LikeList />
+      </div>
     </Fragment>
   );
 };
