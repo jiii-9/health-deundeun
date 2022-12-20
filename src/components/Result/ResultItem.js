7214fa44763a747f5328b770325643fce4bc5d5c
jiii-9 <succeed.g9@gmail.com>
2022-12-20 16:55:33 +0900

    clean up repeated tags

--- src/components/Result/ResultItem.js
+++ src/components/Result/ResultItem.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import HeartButton from "../UI/HeartButton";
 
 import classes from "./ResultItem.module.css";
 
@@ -13,42 +14,69 @@ const ResultItem = ({
   cholesterol,
   transFat,
 }) => {
+  const nutrientList = [
+    {
+      nutrientName: "열량",
+      nutrientMark: kcal,
+    },
+    {
+      nutrientName: "탄수화물",
+      nutrientMark: carbohydrate,
+    },
+    {
+      nutrientName: "단백질",
+      nutrientMark: protein,
+    },
+    {
+      nutrientName: "지방",
+      nutrientMark: fat,
+    },
+    {
+      nutrientName: "당류",
+      nutrientMark: sugars,
+    },
+    {
+      nutrientName: "나트륨",
+      nutrientMark: sodium,
+    },
+    {
+      nutrientName: "콜레스테롤",
+      nutrientMark: cholesterol,
+    },
+    {
+      nutrientName: "트렌스지방",
+      nutrientMark: transFat,
+    },
+  ];
+
+  const [isClicked, setIsClicked] = useState(false);
+
+  const clickHeartHandler = () => {
+    setIsClicked(!isClicked);
+    console.log(isClicked);
+  };
+
   return (
     <div className={classes["result-item"]}>
-      <span className={classes["food-name"]}>{name}</span>
+      <div className={classes.wrapper}>
+        <span className={classes["food-name"]}>{name}</span>
+        <HeartButton
+          className={
+            isClicked ? classes["active-heart"] : classes["heart-icon"]
+          }
+          isClicked={isClicked}
+          onClick={clickHeartHandler}
+        />
+      </div>
       <div className={classes["result-info"]}>
-        <div className={classes["item-info"]}>
-          <span className={classes["nutrient-name"]}>열량</span>
-          <span className={classes.amount}>{kcal}</span>
-        </div>
-        <div className={classes["item-info"]}>
-          <span className={classes["nutrient-name"]}>탄수화물</span>
-          <span className={classes.amount}>{carbohydrate}</span>
-        </div>
-        <div className={classes["item-info"]}>
-          <span className={classes["nutrient-name"]}>단백질</span>
-          <span className={classes.amount}>{protein}</span>
-        </div>
-        <div className={classes["item-info"]}>
-          <span className={classes["nutrient-name"]}>지방</span>
-          <span className={classes.amount}>{fat}</span>
-        </div>
-        <div className={classes["item-info"]}>
-          <span className={classes["nutrient-name"]}>당류</span>
-          <span className={classes.amount}>{sugars}</span>
-        </div>
-        <div className={classes["item-info"]}>
-          <span className={classes["nutrient-name"]}>나트륨</span>
-          <span className={classes.amount}>{sodium}</span>
-        </div>
-        <div className={classes["item-info"]}>
-          <span className={classes["nutrient-name"]}>콜레스테롤</span>
-          <span className={classes.amount}>{cholesterol}</span>
-        </div>
-        <div className={classes["item-info"]}>
-          <span className={classes["nutrient-name"]}>트렌스지방</span>
-          <span className={classes.amount}>{transFat}</span>
-        </div>
+        {nutrientList.map((nutrientItem, idx) => (
+          <div className={classes["item-info"]} key={idx}>
+            <span className={classes["nutrient-name"]}>
+              {nutrientItem.nutrientName}
+            </span>
+            <span className={classes.amount}>{nutrientItem.nutrientMark}</span>
+          </div>
+        ))}
       </div>
     </div>
   );
