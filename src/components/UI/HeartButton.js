aca7cdc610d9246f7ec5e75155c3e6749c13c74a
jiii-9 <succeed.g9@gmail.com>
2022-12-20 16:58:45 +0900

    add filled heart icon

--- src/components/UI/HeartButton.js
+++ src/components/UI/HeartButton.js
@@ -1,15 +1,15 @@
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
+import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
 
-import classes from "./HeartButton.module.css";
-
-const HeartButton = () => {
-  // 하트버튼을 누르면 채워진 하트로 바뀌고 해당 데이터가 저장되는 함수 만들어야됨
-
+const HeartButton = props => {
   return (
-    <div>
-      <FontAwesomeIcon className={classes["heart-icon"]} icon={faHeart} />
-    </div>
+    <FontAwesomeIcon
+      className={props.className}
+      icon={props.isClicked ? solidHeart : regularHeart}
+      onClick={props.onClick}
+    />
   );
 };
 
