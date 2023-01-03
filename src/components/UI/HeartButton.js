25f22e84031ea670ecf9f1a3cd64888bb34f416e
jiii-9 <succeed.g9@gmail.com>
2023-01-03 13:14:07 +0900

    change icon name

--- src/components/UI/HeartButton.js
+++ src/components/UI/HeartButton.js
@@ -1,14 +1,14 @@
 import React from "react";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
 import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
 
-const HeartButton = props => {
+const HeartButton = ({ className, isClicked }) => {
   return (
     <FontAwesomeIcon
-      className={props.className}
-      icon={props.isClicked ? solidHeart : regularHeart}
-      onClick={props.onClick}
+      className={className}
+      icon={isClicked ? solidHeart : regularHeart}
     />
   );
 };
