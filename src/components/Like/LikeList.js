fccc6aa017a4d3fb89da8324bf7d5f9f52fd237c
jiii-9 <succeed.g9@gmail.com>
2023-01-03 15:59:38 +0900

    add a button to return to home and add styling

--- src/components/Like/LikeList.js
+++ src/components/Like/LikeList.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import LikeItem from "./LikeItem";
 
 import classes from "./LikeList.module.css";
 
 const LikeList = () => {
+  const navigate = useNavigate();
   let state = useSelector(state => state);
   const likeList = state.like;
 
+  const goHomeHandler = () => {
+    navigate("/", { replace: true });
+  };
+
   if (likeList.length === 0) {
     return (
-      <span className={classes["like-none"]}>좋아요 내역이 없습니다.</span>
+      <div className={classes["none-wrapper"]}>
+        <span className={classes["like-none"]}>좋아요 내역이 없습니다.</span>
+        <button className={classes["return-btn"]} onClick={goHomeHandler}>
+          돌아가기
+        </button>
+      </div>
     );
   }
 
