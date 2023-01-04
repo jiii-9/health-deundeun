723c51851b9648865279ccefa91116b4bede21fd
jiii-9 <succeed.g9@gmail.com>
2023-01-04 17:19:02 +0900

    change the side menu category to component

--- src/components/Layout/SideMenu/SideMenu.js
+++ src/components/Layout/SideMenu/SideMenu.js
@@ -1,20 +1,61 @@
 import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 import classes from "./SideMenu.module.css";
 import SideMenuItem from "./SideMenuItem";
 
-const SideMenu = props => {
-  const sideMenuItem = ["채소", "육류", "생성", "과일", "샐러드"];
+const SideMenu = ({ isOpen, onFlip }) => {
+  const sideMenuItem = [
+    {
+      id: "veggie",
+      name: "채소",
+      detail: [
+        ["고구마", "감자", "당근"],
+        ["시금치", "쌈채소", "나물"],
+        ["브로콜리", "파프리카", "양배추"],
+        ["오이", "호박"],
+        ["콩나물", "버섯"],
+      ],
+    },
+    {
+      id: "meat",
+      name: "육류",
+      detail: [
+        "소고기",
+        "돼지고기",
+        "계란류",
+        ["닭", "오리고기"],
+        ["양념육", "돈까스"],
+        "양고기",
+      ],
+    },
+    {
+      id: "fish",
+      name: "생선",
+      detail: [
+        ["오징어", "낙지", "문어"],
+        ["새우", "게", "랍스터"],
+        ["해산물", "조개"],
+        ["김", "미역"],
+      ],
+    },
+    {
+      id: "fruit",
+      name: "과일",
+      detail: [["사과", "배", "참외"], ["귤", "자몽", "키위"], "딸기"],
+    },
+    {
+      id: "salad",
+      name: "샐러드",
+      detail: ["닭가슴살", "도시락", "파스타"],
+    },
+  ];
 
-  let isOpen = props.isOpen;
   const closeMenuHandler = () => {
-    props.onFlip(isOpen => !isOpen);
+    onFlip(isOpen => !isOpen);
   };
-
-  console.log(isOpen);
 
   return (
     <aside className={classes.sidemenu}>
@@ -33,48 +74,20 @@ const SideMenu = props => {
 
           <ul className={classes["category-list"]}>
             {sideMenuItem.map(item => (
-              <SideMenuItem>
-                <span className={classes["item-name"]}>{item}</span>
-                <FontAwesomeIcon
-                  className={classes["item-icon"]}
-                  icon={faChevronDown}
-                />
-              </SideMenuItem>
+              <SideMenuItem
+                id={item.id}
+                name={item.name}
+                detailMenu={item.detail}
+                key={item.id}
+              />
             ))}
-            {/* <li className={classes["category-item"]}>
-              <span className={classes["item-name"]}>육류</span>
-              <FontAwesomeIcon
-                className={classes["item-icon"]}
-                icon={faChevronDown}
-              />
-            </li>
-            <li className={classes["category-item"]}>
-              <span className={classes["item-name"]}>생선</span>
-              <FontAwesomeIcon
-                className={classes["item-icon"]}
-                icon={faChevronDown}
-              />
-            </li>
-            <li className={classes["category-item"]}>
-              <span className={classes["item-name"]}>과일</span>
-              <FontAwesomeIcon
-                className={classes["item-icon"]}
-                icon={faChevronDown}
-              />
-            </li> */}
           </ul>
         </div>
         <div className={classes.blank}></div>
         <div className={classes["category-wrapper"]}>
           <h3 className={classes["category-name"]}>레시피</h3>
           <ul className={classes["category-list"]}>
-            <SideMenuItem>
-              <span className={classes["item-name"]}>샐러드</span>
-              <FontAwesomeIcon
-                className={classes["item-icon"]}
-                icon={faChevronDown}
-              />
-            </SideMenuItem>
+            <li className={classes["category-item"]}>샐러드</li>
           </ul>
         </div>
       </div>
