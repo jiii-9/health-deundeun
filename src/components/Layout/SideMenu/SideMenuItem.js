41b3c77edaab3b201ded6379e35b8030f5e626c5
jiii-9 <succeed.g9@gmail.com>
2023-01-04 17:20:33 +0900

    add function to expand when category is clicked

--- src/components/Layout/SideMenu/SideMenuItem.js
+++ src/components/Layout/SideMenu/SideMenuItem.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 import classes from "./SideMenuItem.module.css";
+import DetailMenu from "./DetailMenu";
 
-const SideMenuItem = ({ children }) => {
-  return <li className={classes["category-item"]}>{children}</li>;
+const SideMenuItem = ({ id, name, detailMenu }) => {
+  const [isOpenItem, setIsOpenItem] = useState(false);
+
+  const itemFlipHandler = () => {
+    setIsOpenItem(isOpenItem => !isOpenItem);
+  };
+
+  console.log(isOpenItem);
+
+  return (
+    <Fragment>
+      <li className={classes["category-item"]} onClick={itemFlipHandler}>
+        <div>
+          <span className={classes["item-name"]}>{name}</span>
+          <FontAwesomeIcon
+            className={classes["item-icon"]}
+            icon={!isOpenItem ? faChevronDown : faChevronUp}
+          />
+        </div>
+
+        {isOpenItem ? <DetailMenu name={detailMenu} /> : null}
+      </li>
+    </Fragment>
+  );
 };
 
 export default SideMenuItem;
