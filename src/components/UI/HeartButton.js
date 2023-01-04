1253ff1ceb8784544de58ccbd95629cac96f0ba8
jiii-9 <succeed.g9@gmail.com>
2023-01-04 11:23:06 +0900

    add ID to heart icon component

--- src/components/UI/HeartButton.js
+++ src/components/UI/HeartButton.js
@@ -4,9 +4,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
 import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
 
+import classes from "./HeartButton.module.css";
+
 const HeartButton = ({ className, isClicked }) => {
   return (
     <FontAwesomeIcon
+      id={classes["heart-icon"]}
       className={className}
       icon={isClicked ? solidHeart : regularHeart}
     />
